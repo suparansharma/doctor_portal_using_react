4396a2a18f5507946d34726368bb21386a7b02f5
suparansharma <suparansharmap@gmail.com>
2022-10-28 01:02:58 +0600

    primary lvl done

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -13,8 +13,8 @@ const MakeAppointment = () => {
             <div className="flex-1">
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
                 <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
-               <PrimaryButton>GET STARTED</PrimaryButton>
+                <p className='text-white mt-3'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+               <PrimaryButton className='mt-2'>GET STARTED</PrimaryButton>
             </div>
             
         </section>
