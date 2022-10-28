60bbb6ca53147efaefad397fc6fec58c64e52dca
suparansharma <suparansharmap@gmail.com>
2022-10-28 20:04:38 +0600

    uptodate

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
+import Footer from '../Shared/Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import ServiceDetails from './ServiceDetails';
@@ -13,6 +15,8 @@ const Home = () => {
             <Services/>
             <ServiceDetails/>
             <MakeAppointment/>
+            <Contact/>
+            <Footer/>
         </div>
     );
 };
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -10,10 +10,10 @@ const MakeAppointment = () => {
             <img className='mt-[-150px]' src={doctor} alt="" srcset="" />
             </div>
 
-            <div className="flex-1">
+            <div className="flex-1 px-5">
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
-                <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
-                <p className='text-white mt-3'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <h2 className='text-3xl text-white py-5'>Make an Appointment Today</h2>
+                <p className='text-white mt-3 pb-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                <PrimaryButton className='mt-2'>GET STARTED</PrimaryButton>
             </div>
             
