90e5236d30897244abec999e04acd0621238a1c0
suparansharma <suparansharmap@gmail.com>
2022-10-30 14:02:39 +0600

    login pages

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>this is login page</h2>
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  if (user) {
+    console.log(user);
+  }
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold">Login</h2>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  pattern: {
+                    value: /[A-Za-z]{3}/,
+                    message: 'error message' // JS only: <p>error message</p> TS only support string
+                  }})}
+              />
+              <label className="label">
+                <span className="label-text-alt">Alt label</span>
+              </label>
+            </div>
+
+            <input
+              
+              aria-invalid={errors.firstName ? "true" : "false"}
+            />
+            {errors.firstName?.type === "required" && (
+              <p role="alert">First name is required</p>
+            )}
+
+            <input
+              {...register("mail", { required: "Email Address is required" })}
+              aria-invalid={errors.mail ? "true" : "false"}
+            />
+            {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+
+            <input type="submit" />
+          </form>
+
+          <div className="divider">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline"
+          >
+            Continue With Google
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
