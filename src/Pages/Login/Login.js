1a3c887b986611987deee86f3bdd936183f128db
suparansharma <suparansharmap@gmail.com>
2022-10-30 18:21:15 +0600

    validation done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
+import Loading from "../Shared/Loading";
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
 
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  let signInError;
+
+  if(loading || gLoading){
+   return <Loading></Loading>
+  }
+
+  if(error || gError){
+    signInError =<p className="text-red-500"><small>{error?.message || gError?.message}</small></p>
+  }
+
+  
+    if (gUser) {
+      console.log(gUser);
+    }
   const onSubmit = (data) => {
     console.log(data);
+    signInWithEmailAndPassword(data.email,data.password);
   };
-  if (user) {
-    console.log(user);
-  }
+
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
@@ -33,31 +54,47 @@ const Login = () => {
                 placeholder="Your Email"
                 className="input input-bordered w-full max-w-xs"
                 {...register("email", {
+                  required:{
+                    value:true,
+                    message:'Email is Required'
+                  },
                   pattern: {
-                    value: /[A-Za-z]{3}/,
-                    message: 'error message' // JS only: <p>error message</p> TS only support string
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: 'Provide a valid Email' // JS only: <p>error message</p> TS only support string
                   }})}
               />
-              <label className="label">
-                <span className="label-text-alt">Alt label</span>
-              </label>
+               {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+               {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+
+            
             </div>
 
-            <input
-              
-              aria-invalid={errors.firstName ? "true" : "false"}
-            />
-            {errors.firstName?.type === "required" && (
-              <p role="alert">First name is required</p>
-            )}
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Your Password"
+                className="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required:{
+                    value:true,
+                    message:'Password is Required'
+                  },
+                  minLength: {
+                    value: 6,
+                    message: 'Must be 6 characters or longer ' // JS only: <p>error message</p> TS only support string
+                  }})}
+              />
+               {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+               {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
 
-            <input
-              {...register("mail", { required: "Email Address is required" })}
-              aria-invalid={errors.mail ? "true" : "false"}
-            />
-            {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+           
+            </div>
 
-            <input type="submit" />
+            {signInError}
+            <input  className="btn w-full max-w-xs text-white" type="submit" value="Login" />
           </form>
 
           <div className="divider">OR</div>
