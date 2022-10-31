6636615c9c9dbf25a195f91d390b6bed65aca2b5
suparansharma <suparansharmap@gmail.com>
2022-10-31 10:03:28 +0600

    uptodate

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,6 +4,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -25,6 +26,13 @@ const Login = () => {
   const location = useLocation();
   let from = location.state?.from?.pathname || "/";
 
+  useEffect(()=>{
+    if (user || gUser) {
+      navigate(from,{replace:true});
+      console.log(user ||gUser);
+    }
+  },[user,gUser,from,navigate])
+
   if(loading || gLoading){
    return <Loading></Loading>
   }
@@ -34,10 +42,7 @@ const Login = () => {
   }
 
   
-    if (user || gUser) {
-      navigate(from,{replace:true});
-      console.log(user ||gUser);
-    }
+
   const onSubmit = (data) => {
     console.log(data);
     signInWithEmailAndPassword(data.email,data.password);
