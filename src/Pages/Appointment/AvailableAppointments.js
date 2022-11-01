c22f751274c702d7d9bb776bb7b3d69b68315218
suparansharma <suparansharmap@gmail.com>
2022-11-02 01:56:42 +0600

    url change

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -6,9 +6,10 @@ import BookingModal from './BookingModal';
 
 const AvailableAppointments = ({date}) => {
     const [services,setServices] = useState([]);
-    const [treatment,setTreatment] = useState(null)
+    const [treatment,setTreatment] = useState(null);
+    const formattedDate = format(date,'PP');
     useEffect(()=>{
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formattedDate}`)
         .then(res =>res.json())
         .then(data =>setServices(data))
     })
