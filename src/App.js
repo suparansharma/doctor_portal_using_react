41c5d3e42f4c3ae5f3af65e351ed72df9b0cf81d
suparansharma <suparansharmap@gmail.com>
2022-11-05 23:31:41 +0600

    table

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import RequireAuth from './Pages/Login/RequireAuth';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import MyAppointments from './Pages/Dashboard/MyAppointments';
+import MyReview from './Pages/Dashboard/MyReview';
+import MyHistory from './Pages/Dashboard/MyHistory';
 function App() {
   return (
     <div className='max-w-7xl mx-auto px-12' >
@@ -21,6 +25,15 @@ function App() {
         <RequireAuth>
           <Appointment/>
           </RequireAuth>} />
+
+          <Route path="dashboard" element={
+        <RequireAuth>
+          <Dashboard/>
+          </RequireAuth>} >
+            <Route index element={<MyAppointments></MyAppointments>}></Route>
+            <Route path='review' element={<MyReview></MyReview>}></Route>
+            <Route path='history' element={<MyHistory></MyHistory>}></Route>
+          </Route>
         <Route path="login" element={<Login/>} />
         <Route path="signup" element={<SignUp/>} />
       </Routes>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -18,6 +18,9 @@ const Navbar = () => {
      <li><Link to="/review">Review</Link></li>
      <li><Link to="/contact">Contact</Link></li>
      <li><Link to="/about">About</Link></li>
+     {
+      user &&  <li><Link to="/dashboard">Dashboard</Link></li>
+     }
      {/* <li><Link to="/login">Login</Link></li> */}
      <li>{user ? <button className="btn btn-ghost" onClick={logout}>Sign Out</button> :<Link to="/login">Login</Link>}</li>
 </>
@@ -38,6 +41,13 @@ const Navbar = () => {
           <ul class="menu menu-horizontal p-0">
             {menuItems}
           </ul>
+        </div>
+
+        <div className="navbar-end">
+        <label tabIndex="1" htmlFor="dashboard-sidebar" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+        
         </div>
        
       </div>
